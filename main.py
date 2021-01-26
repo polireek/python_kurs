92b0712ebd69b4be97178cf635891e4e0e8265eb
Poli ReeK <polireek@gmail.com>
2021-01-26 14:50:11 +0200

    Вроде все исправил

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import requests
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder = "template")
 
 def save_to_history(currency_to, exchange_rate, amount, result):
-    f = open('history.txt', 'a+')
-    f.write(f"{currency_to},{exchange_rate},{amount},{result}\n")
-    f.close()
+    with open('history.txt', 'a+') as f:
+        f.write(f"{currency_to}, {exchange_rate}, {amount}, {result}\n")
 
-
-def what_kurs(from_m, to_m, money):
+def convert_amount(from_m, to_m, money):
     response = requests.get(f'https://api.exchangeratesapi.io/latest?base={from_m}')
     response_json = response.json()
     rates = response_json['rates']
@@ -18,34 +15,26 @@ def what_kurs(from_m, to_m, money):
     save_to_history(to_m, rates[to_m], money, result)
     return result
 
-
 @app.route('/eur_to_usd/<int:amount>')
 def eur_to_usd(amount):
-    usd = what_kurs("EUR", "USD", amount)
+    usd = convert_amount("EUR", "USD", amount)
     return f"{usd} USD"
-
 
 @app.route("/eur_to_gbp/<int:amount>")
 def eur_to_gbp(amount):
-    usd = what_kurs("EUR", "GBP", amount)
+    usd = convert_amount("EUR", "GBP", amount)
     return f"{usd} GBP"
-
 
 @app.route("/eur_to_php/<int:amount>")
 def eur_to_php(amount):
-    usd = what_kurs("EUR", "PHP", amount)
+    usd = convert_amount("EUR", "PHP", amount)
     return f"{usd} PHP"
-
 
 @app.route("/history")
 def history():
-    f = open('history.txt')
-    history = ""
-    for line in f:
-        history += line + "</br>"
-    f.close()
-    return history
-
+    with open('history.txt', 'r') as f:
+        response = f.read().splitlines()
+    return render_template('history.html', full_history=response)
 
 if __name__ == "__main__":
     app.run()
